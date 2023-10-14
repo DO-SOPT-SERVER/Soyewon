e8d438cb1369e940feeaf5218061b92fd88761fc
Yewon So <soyw96@gmail.com>
2023-10-14 16:38:11 +0900

    git init

---
+++ semi_2/src/main/java/com/example/semi_2/Semi2Application.java
@@ -0,0 +1,13 @@
+package com.example.semi_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Semi2Application.class, args);
+	}
+
+}
